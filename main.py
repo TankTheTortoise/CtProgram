a8f008bfb37c4993267c042e8eb6342344e2a781
Seamus Allison <smallison3409@gmail.com>
2024-04-03 14:28:55 -0400

    Added mean squared error to calculate the loss for the eventual program. It takes in two lists and finds the mse. It has no errors, but returns an exception when the lengths of the lists don't match.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def mse(y, y_hat):
+    if len(y) != len(y_hat):
+        raise Exception("Y and Y_hat aren't the same length")
+    else:
+        sum = 0
+        for i in range(len(y)):
+            sum += (y[i] - y_hat[i]) ** 2
+        sum /= len(y)
+        return sum
