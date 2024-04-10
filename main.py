7b66ff311d8ac83fd75b7fd90096563fae09fb71
Seamus Allison <smallison3409@gmail.com>
2024-04-10 14:36:29 -0400

    Day 3

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def mse(y, y_hat, d=False):
     if len(y) != len(y_hat):
         raise Exception("Y and Y_hat aren't the same length")
     else:
+        sum = 0
+        for i in range(len(y)):
+            sum += (y[i] - y_hat[i]) ** 2
+        sum /= len(y)
+        return sum
         if not d:
             sum = 0
             for i in range(len(y)):
@@ -18,13 +23,37 @@ def mse(y, y_hat, d=False):
             return sum
 
 
-def forward(X, m):
-    for i in range(X):
-        X[i] *= m
+def forward(X, m, b):
+    for i in range(len(X)):
+        X[i] *= m + b
+    return X
 
 
-data1 = [1, 2, 3]
-data2 = [4, 5, 6]
+data1 = [1, 2, 3, 4]
+data2 = [2, 4, 6, 8]
+
+weight = 1
+bias = 1
+
+epochs = 1
+lr = 1
+batch_size = 4
+
+# Data Seperated into minibatches
+data1_batched = []
+for i in range(0, len(data1), batch_size):
+    data1_batched.append(data1[i:i + batch_size])
+print(data1_batched)
+for epoch in range(epochs):
+    train_y = forward(data1, weight, bias)
+    for data in data1_batched:
+        w_error = 0
+        b_error = 0
+        for i in range(len(data)):
+            w_error -= data1[i] * mse(data2[i], train_y[i], d=True)
+            b_error -= mse(data2[i], train_y[i], d=True)
+        weight -= w_error / batch_size
+        bias -= b_error / batch_size
 
 print(mse(data1, data2))
 print(mse(data1, data2, d=True))
