f01be516173ce3cc006e1e9ad025742f0c169d8e
Seamus Allison <smallison3409@gmail.com>
2024-04-12 14:37:43 -0400

    Day 4

--- main.py
+++ main.py
@@ -1,59 +1,48 @@
 def mse(y, y_hat, d=False):
-    if len(y) != len(y_hat):
-        raise Exception("Y and Y_hat aren't the same length")
-    else:
-        sum = 0
-        for i in range(len(y)):
-            sum += (y[i] - y_hat[i]) ** 2
-        sum /= len(y)
-        return sum
-        if not d:
-            sum = 0
-            for i in range(len(y)):
-                sum += (y[i] - y_hat[i]) ** 2
-            sum /= len(y)
-            return sum
-        if d:
-            sum = 0
-            for i in range(len(y)):
-                sum += y[i] - y_hat[i]
-
-            sum /= len(y)
-            sum *= 2
-            return sum
+    if not d:
+        return (y_hat-y) ** 2
+    if d:
+        return 2 * (y_hat - y)
 
 
-def forward(X, m, b):
-    for i in range(len(X)):
-        X[i] *= m + b
-    return X
+def forward(x, m, b):
+    x = x * m + b
+    return x
+
+
+def element_mul(list1, scalar):
+    for i in range(len(list1)):
+        list1[i] *= scalar
+    return list1[i]
 
 
 data1 = [1, 2, 3, 4]
 data2 = [2, 4, 6, 8]
 
 weight = 1
-bias = 1
+bias = 0.01
 
-epochs = 1
-lr = 1
-batch_size = 4
+epochs = 1000
+lr = 0.001
 
-# Data Seperated into minibatches
-data1_batched = []
-for i in range(0, len(data1), batch_size):
-    data1_batched.append(data1[i:i + batch_size])
-print(data1_batched)
+batch_size=2
+batched_data1 = []
+for d in range(batch_size, len(data1)+1):
+    batched_data1.append(data1[d-batch_size: d])
+print(batched_data1)
+samples = len(data1)
 for epoch in range(epochs):
-    train_y = forward(data1, weight, bias)
-    for data in data1_batched:
-        w_error = 0
-        b_error = 0
-        for i in range(len(data)):
-            w_error -= data1[i] * mse(data2[i], train_y[i], d=True)
-            b_error -= mse(data2[i], train_y[i], d=True)
-        weight -= w_error / batch_size
-        bias -= b_error / batch_size
+    for i in range(0, samples):
+        output = 0
+        error = 0
+        for j in range(batch_size):
+            output += forward(data1[i], weight, bias)
+            error += mse(data2[i], output, d=True)
+        weight -= (error * data1[i] * lr)
+        bias -= (error * lr)/batch_size
+print(weight, bias)
 
-print(mse(data1, data2))
-print(mse(data1, data2, d=True))
+print(weight, bias)
+for j in range(len(data1)):
+    data1[j] = data1[j] * weight + bias
+print(data1)
