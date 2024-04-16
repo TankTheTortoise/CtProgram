3914fb43e07436dfe6d00f0551b28dba9671313c
Seamus Allison <smallison3409@gmail.com>
2024-04-16 14:36:51 -0400

    Day6

--- main.py
+++ main.py
@@ -1,48 +1,61 @@
 def mse(y, y_hat, d=False):
     if not d:
-        return (y_hat-y) ** 2
+        return (y_hat - y) ** 2
     if d:
         return 2 * (y_hat - y)
 
 
-def forward(x, m, b):
-    x = x * m + b
-    return x
+with open("trainY.txt", "r") as testX_file:
+    testX = testX_file.readline()
+testX = testX.split(",")
+for i in range(len(testX)):
+    testX[i] = float(testX[i])
 
+with open("testY.txt", "r") as testY_file:
+    testY = testY_file.readline()
+testY = testY.split(",")
+for i in range(len(testY)):
+    testY[i] = float(testY[i])
 
-data1 = [x for x in range(4)]
-data2 = [x*2+3 for x in range(4)]
+with open("trainX.txt", "r") as trainX_file:
+    trainX = trainX_file.readline()
+trainX = trainX.split(",")
+for i in range(len(trainX)):
+    trainX[i] = float(trainX[i])
+
+with open("testX.txt", "r") as trainY_file:
+    trainY = trainY_file.readline()
+trainY = trainY.split(",")
+for i in range(len(trainY)):
+    trainY[i] = float(trainY[i])
 
 weight = 2
 bias = 0.01
 
 epochs = 1000
-lr = 0.01
+lr = 0.001
 
-batch_size=2
+batch_size = 1
 batched_data1 = []
 batched_data2 = []
-for d in range(batch_size, len(data1)+1):
-    batched_data1.append(data1[d-batch_size: d])
-for d in range(batch_size, len(data2)+1):
-    batched_data2.append(data2[d-batch_size: d])
-
-print(batched_data1)
-samples = len(batched_data1)
+samples = len(trainX)
 for epoch in range(epochs):
-    for i in range(0, samples):
+    for i in range(0, samples, batch_size):
         output = 0
         error = 0
-        for j in range(0, int(len(data1)/batch_size)-1):
-            output += forward(batched_data1[i][j], weight, bias)
-            error += mse(batched_data2[i][j], output, d=True)
-        weight -= (error * data1[i] * lr)
-        bias -= (error * lr)/batch_size
+        w_error = 0
+        for j in range(0, batch_size):
+            output += trainX[i + j] * weight + bias
+            error += mse(trainY[i + j], output, d=True)
+            w_error += mse(trainY[i + j], output, d=True) * trainX[i + j]
 
-    print(weight, bias)
+        weight -= (lr * w_error) / batch_size
+        bias -= (error * lr) / batch_size
+        print(weight, bias)
 
-weight = round(weight, 4)
-bias = round(bias, 4)
-for j in range(len(data1)):
-    data1[j] = round(data1[j] * weight + bias, 4)
-print(data1)
+true = 0
+for j in range(len(trainX)):
+    if testY[j] * 0.95 < round(testX[j] * weight + bias, 2) < testY[j] * 1.05:
+        true += 1
+
+print(f"{true}/{len(testX)} correct.")
